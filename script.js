fd169cfe561d05f36213755b87cbd7a9241a58e2
jamshidakramovich14 <jamshidturdiyev103>
2024-01-23 08:01:19 +0500

    all files

---
+++ script.js
@@ -0,0 +1,128 @@
+4 - masala;
+
+// function Summa(n){
+//         let arr = []
+//         for(let  i = 0; i <= n; i++){
+//             arr.push(i*10)
+//         }
+
+//         let summa = 0
+
+//         for(let i = 0; i < arr.length; i++){
+//             if(i % 2 == 1 ){
+//                 summa += arr[i]
+//                 console.log(arr[i]);
+//             }
+//         }
+
+//         return summa
+// }
+
+// let result = Summa(6)
+
+// 5 - masala
+
+// function addNumbers(n){
+//     let arr = []
+//     for(let i = 0; i < n; i++){
+//         arr.push(i)
+//     }
+
+//     return arr
+// }
+
+// let result = addNumbers(5)
+// console.log(result)
+
+//  6- masala
+
+// function addNumbers(n) {
+//   let arr = [];
+//   for (let i = n; i >= 0; i--) {
+//     arr.push(i);
+//   }
+
+//   return arr;
+// }
+
+// let result = addNumbers(5);
+// console.log(result);
+
+//  7 - masala
+
+// let a = []
+// let b = []
+// let c = []
+// a["ismi"] = "Abdulloh";
+// a["yoshi"] = 20;
+// b["ismi"] = "Elbek";
+// b["yoshi"] = 20;
+
+// console.log(
+//   `${a["ismi"]} ni ${b["ismi"]} dan yosh farqi ${
+//     Math.abs(a["yoshi"]) - (b["yoshi"])
+//   }`
+// );
+
+//  12 - masala
+// function devision(n){
+
+//     let arr = [];
+//     let sum = 0;
+//     for (let i = 0; i <= n; i++) {
+//       sum += i;
+//     }
+//     sum = sum / 2
+//     return sum
+// }
+
+// let result = devision(7)
+// console.log(result)
+
+//  13 - masala
+
+// function Number(n){
+//     let arr = []
+//     for(let i = 0; i <= n; i++){
+//         if(i % 2 == 1)[
+//             arr.push(i)
+//         ]
+//     }
+//     return arr
+// }
+
+// let result = Number(9);
+// console.log(result)
+
+// 14 - masala
+
+// function Number(n) {
+//   let arr = [];
+// //   let dev = 0;
+//   for (let i = 0; i <= n; i++){
+//     for(let j = 0; j <= i; j++){
+//         if(i % j == 0 ){
+//             arr.push(j);
+//         }
+//     }
+//   }
+
+//   return arr
+
+// }
+
+// // let result = Number(9);
+// // console.log(result);
+
+//  15 - masala
+
+// function Number(n) {
+//   let arr = [];
+//   for (let i = 0; i <= n; i++) {
+//     if (i % 2 == 1) [arr.push(i)];
+//   }
+//   return arr;
+// }
+
+// let result = Number(9);
+// console.log(result);
